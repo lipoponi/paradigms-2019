7116a0d0dc0745d10cddeb85d97808d13be6d7ba
Stanislav Tebloev <stas.tebloev@yandex.ru>
2019-04-02 00:08:51 +0300

    First working version

--- task-7/src/linear.clj
+++ task-7/src/linear.clj
@@ -1,9 +1,23 @@
-(defn v+ [& xs] (apply map + xs))
-(defn v- [& xs] (apply map - xs))
-(defn v* [& xs] (apply map * xs))
-(defn scalar [a b] (reduce + (v* a b)))
-(defn v*s [v & xs] (map #(* % (reduce * xs)) v))
+(defn =count? [xs] (apply = (mapv count xs)))
 
-(defn m+ [& xs] (apply map v+ xs))
-(defn m- [& xs] (apply map v- xs))
-(defn m* [& xs] (apply map v* xs))
+(defn vv [f] (fn [& xs] {:pre [(=count? xs)]}
+               (apply mapv f xs)))
+(defn mm [f] (fn [& xs] {:pre [(=count? xs) (every? =count? xs)]}
+               (apply mapv (vv f) xs)))
+
+(def v+ (vv +))
+(def v- (vv -))
+(def v* (vv *))
+(defn scalar [a b] (reduce + (v* a b)))
+(defn vect [a b] (vector (- (* (get a 1) (get b 2)) (* (get a 2) (get b 1)))
+                         (- (* (get a 2) (get b 0)) (* (get a 0) (get b 2)))
+                         (- (* (get a 0) (get b 1)) (* (get a 1) (get b 0)))))
+(defn v*s [v & xs] (mapv #(* % (reduce * xs)) v))
+
+(def m+ (mm +))
+(def m- (mm -))
+(def m* (mm *))
+(defn m*s [m & xs] (mapv #(apply v*s % xs) m))
+(defn m*v [m v] (mapv #(scalar v %) m))
+(defn transpose [m] (apply mapv vector m))
+(defn m*m [a b] (mapv #(m*v (transpose b) %) a))
