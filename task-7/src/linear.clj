5de75a949e79c381f70ac687f67f28239206159a
Stanislav Tebloev <stas.tebloev@yandex.ru>
2019-04-01 14:38:51 +0300

    Add `v*s` function

--- task-7/src/linear.clj
+++ task-7/src/linear.clj
@@ -2,6 +2,7 @@
 (defn v- [& xs] (apply map - xs))
 (defn v* [& xs] (apply map * xs))
 (defn scalar [a b] (reduce + (v* a b)))
+(defn v*s [v & xs] (map #(* % (reduce * xs)) v))
 
 (defn m+ [& xs] (apply map v+ xs))
 (defn m- [& xs] (apply map v- xs))
