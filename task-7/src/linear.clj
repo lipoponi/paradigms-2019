5cac15c28f9551d4b90f3bc528ec51f146c27575
Stanislav Tebloev <stas.tebloev@yandex.ru>
2019-04-02 01:12:25 +0300

    Do some upgrades and add arity support

--- task-7/src/linear.clj
+++ task-7/src/linear.clj
@@ -1,4 +1,9 @@
-(defn =count? [xs] (apply = (mapv count xs)))
+(defn =count?
+  ([xs] (apply = (mapv count xs)))
+  ([count xs] (apply #(= count %) (mapv count xs))))
+(defn cyclic-shift [v shift] (subvec (vec (reduce concat (repeat 2 v)))
+                                     (mod shift (count v))
+                                     (+ (mod shift (count v)) (count v))))
 
 (defn vv [f] (fn [& xs] {:pre [(=count? xs)]}
                (apply mapv f xs)))
@@ -9,9 +14,10 @@
 (def v- (vv -))
 (def v* (vv *))
 (defn scalar [a b] (reduce + (v* a b)))
-(defn vect [a b] (vector (- (* (get a 1) (get b 2)) (* (get a 2) (get b 1)))
-                         (- (* (get a 2) (get b 0)) (* (get a 0) (get b 2)))
-                         (- (* (get a 0) (get b 1)) (* (get a 1) (get b 0)))))
+(defn vect
+  ([a] (vec a))
+  ([a b] {:pre [(= 3 (count a) (count b))]} (cyclic-shift (v- (v* a (cyclic-shift b 1)) (v* (cyclic-shift a 1) b)) 1))
+  ([a b & xs] (reduce vect (vect a b) xs)))
 (defn v*s [v & xs] (mapv #(* % (reduce * xs)) v))
 
 (def m+ (mm +))
@@ -20,4 +26,7 @@
 (defn m*s [m & xs] (mapv #(apply v*s % xs) m))
 (defn m*v [m v] (mapv #(scalar v %) m))
 (defn transpose [m] (apply mapv vector m))
-(defn m*m [a b] (mapv #(m*v (transpose b) %) a))
+(defn m*m
+  ([a] a)
+  ([a b] (mapv #(m*v (transpose b) %) a))
+  ([a b & xs] (reduce m*m (m*m a b) xs)))
