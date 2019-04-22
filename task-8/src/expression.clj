30dfba761b5a55714433e5def8a365c044f6691f
Stanislav Tebloev <stas.tebloev@yandex.ru>
2019-04-22 16:41:48 +0300

    Add MinMax modification

--- task-8/src/expression.clj
+++ task-8/src/expression.clj
@@ -2,13 +2,19 @@
 (defn variable [name] (fn [values] (values name)))
 (defn operation [op] (fn [& xs] (fn [vars] (apply op (map #(% vars) xs)))))
 
+(defn div [a b] (/ (double a) b))
+(defn minimum [& xs] (reduce #(min %1 %2) (nth xs 0) xs))
+(defn maximum [& xs] (reduce #(max %1 %2) (nth xs 0) xs))
+
 (def add (operation +))
 (def subtract (operation -))
 (def multiply (operation *))
-(def divide (operation (fn [a b] (/ (double a) b))))
+(def divide (operation div))
 (def negate (operation -))
+(def min (operation minimum))
+(def max (operation maximum))
 
-(def operations {'+ add '- subtract '* multiply '/ divide 'negate negate})
+(def operations {'+ add '- subtract '* multiply '/ divide 'negate negate 'min min 'max max})
 
 
 (defn parse [unit] (cond
