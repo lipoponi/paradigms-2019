0d9be8160883a4b26d4531dac86febd6a99c02c0
Stanislav Tebloev <stas.tebloev@yandex.ru>
2019-04-27 22:16:25 +0300

    Almost done hard modification

--- task-9/src/expression.clj
+++ task-9/src/expression.clj
@@ -1,25 +1,80 @@
-(defprotocol IExpression
-  (^Number evaluate [this vars])
-  (^String toString [this])
-  (^IExpression diff [this variable]))
+(defn get-field [object key]
+  (cond
+    (contains? object key) (object key)
+    (contains? object :prototype) (get-field (object :prototype) key)))
+(defn call-method [object key & args] (apply (get-field object key) (cons object args)))
 
 
-(deftype Constant [^Number x]
-  IExpression
-  (evaluate [this vars] x)
-  (toString [this] (str x))
-  (diff [this variable] (Constant. 0)))
 
-(deftype Variable [^String name]
-  IExpression
-  (evaluate [this vars] (vars name))
-  (toString [this] name)
-  (diff [this variable] (Constant. (if (= name variable) 1 0))))
+(declare Constant Variable Add Subtract Multiply Divide Negate)
+
+(defn evaluate [expression mapping] (call-method expression :evaluate mapping))
+(defn toString [expression] (call-method expression :toString))
+(defn diff [expression diff-variable] (call-method expression :diff diff-variable))
 
 
-(defn evaluate [expression vars] (.evaluate expression vars))
-(defn toString [expression] (.toString expression))
-(defn diff [expression variable] (.diff expression variable))
+(def Constant:prototype
+  {:evaluate (fn [this _] (get-field this :value))
+   :toString (fn [this] (format "%.1f" (get-field this :value)))
+   :diff     (fn [_ _] (Constant 0))})
+(defn Constant [^Number value]
+  {:prototype Constant:prototype
+   :value     (double value)})
+
+(def Variable:prototype
+  {:evaluate (fn [this mapping] (mapping (get-field this :name)))
+   :toString (fn [this] (get-field this :name))
+   :diff     (fn [this diff-variable] (Constant (if (= diff-variable (get-field this :name)) 1 0)))})
+(defn Variable [^String name]
+  {:prototype Variable:prototype
+   :name      name})
+
+(def Operation:prototype
+  {:evaluate-inners (fn [this mapping] (mapv #(evaluate % mapping) (get-field this :inners)))
+   :evaluate        (fn [this mapping] (apply (get-field this :perform) (call-method this :evaluate-inners mapping)))
+   :toString-inners (fn [this] (clojure.string/join " " (mapv #(toString %) (get-field this :inners))))
+   :toString        (fn [this] (str "(" (get-field this :represent) " " (call-method this :toString-inners) ")"))
+   :diff-inners     (fn [this diff-variable] (mapv #(diff % diff-variable) (get-field this :inners)))})
+(defn Operation [custom]
+  (fn [& inners]
+    (merge custom
+           {:prototype Operation:prototype
+            :inners    inners})))
+
+(def Add
+  (Operation
+    {:perform   +
+     :represent "+"
+     :diff      (fn [this diff-variable]
+                  (apply Add (call-method this :diff-inners diff-variable)))}))
+(def Subtract
+  (Operation
+    {:perform   -
+     :represent "-"
+     :diff      (fn [this diff-variable]
+                  (apply Subtract (call-method this :diff-inners diff-variable)))}))
+(def Multiply
+  (Operation
+    {:perform   *
+     :represent "*"
+     :diff      (fn [this diff-variable] nil)}))            ;TODO
+(def Divide
+  (Operation
+    {:perform   (fn [a & xs] (/ (double a) (apply * xs)))
+     :represent "/"
+     :diff      (fn [this diff-variable] nil)}))            ;TODO
+(def Negate
+  (Operation
+    {:perform   -
+     :represent "negate"
+     :diff      (fn [this diff-variable] (apply Negate (call-method this :diff-inners diff-variable)))}))
 
 
-(println (evaluate (diff (Variable. "x") "y") {}))
+
+(def operations {'+ Add '- Subtract '* Multiply '/ Divide 'negate Negate})
+
+(defn parsePostfix [unit] (cond
+                            (number? unit) (Constant unit)
+                            (symbol? unit) (Variable (str unit))
+                            (list? unit) (apply (operations (first unit)) (map parsePostfix (rest unit)))))
+(defn parseObject [expression] (parsePostfix (read-string expression)))
